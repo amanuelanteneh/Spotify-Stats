4b66412576d98cb6f0f73b4593eb57ccbc04b88c
amanuelanteneh <asa2rc@virginia.edu>
2021-07-21 20:51:07 -0400

    start recommendations tab

--- js/index.js
+++ js/index.js
@@ -4,6 +4,9 @@ var songName2 = document.getElementById("songName2");
 var song2 = document.getElementById("song2");
 var songName3 = document.getElementById("songName3");
 var song3 = document.getElementById("song3");
+var songName4 = document.getElementById("songName4");
+var song4 = document.getElementById("song4")
+
 
 song1.addEventListener("mouseover", () => {
 	songName1.style.visibility = "visible";
@@ -17,6 +20,10 @@ song3.addEventListener("mouseover", () => {
 	songName3.style.visibility = "visible";
 });
 
+song4.addEventListener("mouseover", () => {
+	songName4.style.visibility = "visible";
+});
+
 song1.addEventListener("mouseout", () => {
 	songName1.style.visibility = "hidden";
 });
@@ -27,4 +34,8 @@ song2.addEventListener("mouseout", () => {
 
 song3.addEventListener("mouseout", () => {
 	songName3.style.visibility = "hidden";
+});
+
+song4.addEventListener("mouseout", () => {
+	songName4.style.visibility = "hidden";
 });
