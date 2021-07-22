3c2c9cc4211af574aca63357da731e4c15776917
amanuelanteneh <asa2rc@virginia.edu>
2021-07-21 20:36:23 -0400

    start recommendations tab

--- login.php
+++ login.php
@@ -45,6 +45,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             <a class="nav-link" href="playlist.php">Playlist</a>
           </li>
           <li class="nav-item">
+            <a class="nav-link" href="recommendations.php">Recommendations</a>
+          </li>
+          <li class="nav-item">
             <a class="nav-link" href="logout.php">Logout</a>
           </li>
         </ul>
