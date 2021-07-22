d365c9f9b284ace88e2e94946bf996a329e32732
amanuelanteneh <asa2rc@virginia.edu>
2021-07-21 20:46:50 -0400

    start recommendations tab

--- index.php
+++ index.php
@@ -36,6 +36,8 @@
     $song2 = $myRecentSongs->items[1]->track->name;
     $albumCover3 = $myRecentSongs->items[2]->track->album->images[0]->url;
     $song3 = $myRecentSongs->items[2]->track->name;
+    $albumCover4 = $myRecentSongs->items[3]->track->album->images[0]->url;
+    $song4 = $myRecentSongs->items[3]->track->name;
     }
     catch (SpotifyWebAPI\SpotifyWebAPIException $e) {
       header("Location: logout.php");      
@@ -122,6 +124,18 @@
         <div class="carousel-caption">
           <h1>Make a Playlist</h1>
           <h3>Create a playlist based off your top songs and artists!</h3>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <div class="d-flex justify-content-center">
+          <img src=<?php echo $albumCover4 ?> class="d-block" alt="" id="song4">
+        </div>
+        <div class="d-flex justify-content-center">
+           <span class="songName" id="songName4"> <?php echo $song4 ?> </span>
+        </div>
+        <div class="carousel-caption">
+          <h1>Get Recommendations</h1>
+          <h3>Get song recommendations based on artists and songs!</h3>
         </div>
       </div>
     </div>
